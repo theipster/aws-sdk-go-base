1eafe5707518cfd93438b8eb5d8b043b2552f93f
Alan Ip <theipster@users.noreply.github.com>
2023-03-20 01:52:57 +0000

    last duplicated

--- aws_config_test.go
+++ aws_config_test.go
@@ -2955,6 +2955,7 @@ func TestGetAwsConfigWithAccountIDAndPartition(t *testing.T) {
 				servicemocks.MockStsAssumeRoleValidEndpoint,
 				servicemocks.MockStsAssumeRoleValidEndpoint,
 				servicemocks.MockStsGetCallerIdentityValidAssumedRoleEndpoint,
+				servicemocks.MockStsGetCallerIdentityValidAssumedRoleEndpoint,
 			},
 		},
 	}
